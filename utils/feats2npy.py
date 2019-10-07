5ed2d4e24468004ca7fb6579ed1ee1f3c2fb76ee
Ryuichi Yamamoto <zryuichi@gmail.com>
2019-10-07 11:45:42 +0900

    Fix conflict

--- utils/feats2npy.py
+++ utils/feats2npy.py
@@ -1,29 +1,5 @@
 #!/usr/bin/env python
 #  coding: utf-8
-<<<<<<< HEAD
-"""Feats to npy conversion.
-
-usage: feats2npy.py [options] <scp> <out_dir>
-
-options:
-    -h, --help               Show help message.
-"""
-from docopt import docopt
-import sys
-import numpy as np
-from os.path import join
-from kaldiio import ReadHelper
-import os
-
-if __name__ == "__main__":
-    args = docopt(__doc__)
-    scp_file = args["<scp>"]
-    out_dir = args["<out_dir>"]
-    os.makedirs(out_dir, exist_ok=True)
-    with ReadHelper(f"scp:{scp_file}") as f:
-        for utt_id, arr in f:
-            out_path = join(out_dir, f"{utt_id}-feats.npy")
-=======
 
 import argparse
 from kaldiio import ReadHelper
@@ -48,6 +24,5 @@ if __name__ == "__main__":
     with ReadHelper(f"scp:{args.scp_file}") as f:
         for utt_id, arr in f:
             out_path = join(args.out_dir, f"{utt_id}-feats.npy")
->>>>>>> origin/master
             np.save(out_path, arr, allow_pickle=False)
     sys.exit(0)
